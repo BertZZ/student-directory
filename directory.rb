c67ba5e9f6b5ccd6b56ed74138ea3219c3d798b4
BertZZ <bertie.wooles@hotmail.co.uk>
2017-03-30 01:24:14 +0000

    Changes

--- directory.rb
+++ directory.rb
@@ -13,8 +13,19 @@ students = [
 "Norman Bates"
 ]
 
+def print_header
 puts "The students of Villains Academy"
 puts "-------------"
-students.each {|student| puts student}
+end 
 
-puts "Overall, we have #{students.count} great students"
+def print(names)
+    names.each {|name| puts name}
+end 
+
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
