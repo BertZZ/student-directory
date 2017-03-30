5c0f9cf8e4bc34d56a1db7d9f8ef6eac9b788cf8
BertZZ <bertie.wooles@hotmail.co.uk>
2017-03-30 13:55:32 +0000

    Added user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+def input_students
+    puts "please enter the names of the students"
+    puts "To exit, hit return twice"
+    students = []
+    name = $stdin.gets.chomp
+# while the name is not empty, repeat this code
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = $stdin.gets.chomp
+    end
+    students
+end
 
 def print_header
 puts "The students of Villains Academy"
@@ -28,6 +28,7 @@ def print_footer(students)
 puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
