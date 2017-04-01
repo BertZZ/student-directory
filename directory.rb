6258884f2603633d739c329ecb0cc2ac6a288068
BertZZ <bertie.wooles@hotmail.co.uk>
2017-04-01 20:23:48 +0000

    Added save csv functionality

--- directory.rb
+++ directory.rb
@@ -52,6 +52,7 @@ end
 def print_menu
     puts "1. Input the Students"
     puts "2. Print the Students"
+    puts "3. Save Students to students.csv "
     puts "9. Exit"
 end
 
@@ -67,6 +68,8 @@ def process(selection)
             input_students
         when "2"
             show_students
+        when "3"
+            save_students
         when "9"
             exit 
         else
@@ -79,6 +82,16 @@ def interactive_menu
         print_menu
         process(gets.chomp)
     end
+end
+
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student [:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
 end
 
 interactive_menu 
